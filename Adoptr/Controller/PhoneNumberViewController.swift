ff47b9e5664c918c88d21152b7660948a4aeb91c
Alexandra Negru <negru_alexandra90@yahoo.com>
2021-08-01 09:15:27 +0300

    Custom text field for one time code

---
+++ Adoptr/Controller/PhoneNumberViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PhoneNumberViewController.swift
+//  Adoptr
+//
+//  Created by Alexandra Negru on 01/06/2021.
+//
+
+import UIKit
+
+class PhoneNumberViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
